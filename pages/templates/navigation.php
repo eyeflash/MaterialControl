29b279034ba7aa77cbc085521ad267ebacabc3f3
eyeflash <sr7.banjarmasin@gmail.com>
2017-02-02 15:53:32 +0700

    commit pertama

---
+++ pages/templates/navigation.php
@@ -0,0 +1,7 @@
+ <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <?php include 'template_header.php'?>
+            <!-- /.navbar-top-links -->
+
+            <?php include 'template_sidebar.php';?>
+            <!-- /.navbar-static-side -->
+        </nav>
